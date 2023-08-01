16f6a8c580eee7c4d4e57c2a8c87b68410b185a8
时启龙 <shiqilong@beyondcent.com>
2023-08-01 21:00:56 +0800

    📦 NEW: Add three.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import * as Three from 'three'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const scene = new Three.Scene()
+  scene.background = new Three.Color(0x001111)
+  const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5)
+  camera.position.z = 5
+  scene.add(camera)
+  const geometry = new Three.BoxGeometry()
+  const material = new Three.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new Three.Mesh(geometry, material)
+  scene.add(cube)
+  const renderer = new Three.WebGLRenderer({
+    canvas: document.getElementById('three')
+  })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  const render = () => {
+    requestAnimationFrame(render)
+    renderer.render(scene, camera)
+  }
+  requestAnimationFrame(render)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <canvas id="three"></canvas>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
